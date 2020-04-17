785a2006a4b6d0e4c49ce55d66473056c658077b
Alec Dye <alecdye@gmail.com>
2020-04-17 07:24:29 -0700

    Building recipe methods and endpoints

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -12,7 +12,15 @@ function getRecipes() {
 }
 
 // return all ingredients and quantities for recipe_id
-function getShoppingList() {}
+function getShoppingList(id) {
+  return db("recipe_ingredients")
+    .join("recipes", "recipe_ingredients.id", "recipe.id")
+    .join("ingredients", "recipe_ingredients.ingredient_id", "ingredient.id")
+    .select("recipe.name", "ingredient.name", "recipe_ingredient.qauntity")
+    .where("recipe.id", id);
+}
 
 // return instructions with steps for recipe_id
-function getInstructions() {}
+function getInstructions(id) {
+  return db("instructions").where("recipe_id", id);
+}
