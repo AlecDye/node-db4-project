578859d1e7abf8a75f1973fae7c4ba8b2703c69f
Alec Dye <alecdye@gmail.com>
2020-04-16 19:45:13 -0700

    Building migration to create initial tables

---
+++ data/migrations/20200416193107_add_initial_tables.js
@@ -0,0 +1,58 @@
+exports.up = function (knex) {
+  return (
+    knex.schema
+      // Recipes table
+      .createTable("recipes", (tbl) => {
+        tbl.string("id", 255).primary();
+        tbl.string("name", 255).notNullable().unique();
+      })
+      // Instructions table
+      .createTable("instructions", (tbl) => {
+        tbl.string("id", 255).primary();
+        tbl.float("step_order", 255).notNullable();
+        tbl.string("step", 255).notNullable();
+        // Foreign key (recipes)
+        tbl
+          .string("recipe_id", 255)
+          .notNullable()
+          .references("id")
+          .inTable("recipes")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
+      // Ingredients table
+      .createTable("ingredients", (tbl) => {
+        tbl.string("id", 255).primary();
+        tbl.string("name", 255).notNullable();
+      })
+      // Recipe_ingredients (junction) table
+      .createTable("recipe_ingredients", (tbl) => {
+        tbl.string("id", 255).primary();
+        // Foreign keys (recipe & ingredients)
+        tbl
+          .string("recipe_id", 255)
+          .notNullable()
+          .references("id")
+          .inTable("receipes")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        tbl
+          .string("ingredients_id", 255)
+          .notNullable()
+          .references("id")
+          .inTable("ingredients")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        // Quantity (of ingredients)
+        tbl.float("quantity").notNullable();
+      })
+  );
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("instructions")
+    .dropTableIfExists("recipes");
+};
